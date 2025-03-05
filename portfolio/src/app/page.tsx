e9e94f8397ebcab6b1645df47f092f45b68ecfae
Nicolai Harms <Nicolai.harms@gmail.com>
2025-03-05 14:39:08 +0100

    Temp removal of Dark / lightmode

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -4,7 +4,7 @@ import { useSmoothScroll } from "@/hooks/useSmoothScroll";
 import { useScrollToTop } from "@/hooks/useScrollToTop";
 import { Hero } from "@/components/sections/Hero";
 import { TechStack } from "@/components/sections/TechStack";
-import Darkmode from "@/components/ui/dark-toggle";
+// import Darkmode from "@/components/ui/dark-toggle";
 import FeaturedCases from "@/components/sections/FeaturedCases";
 import { Header } from "@/components/ui/Header";
 import AboutMe from "@/components/sections/AboutMe";
@@ -19,9 +19,7 @@ export default function Home() {
       <TechStack />
       <FeaturedCases />
       <AboutMe />
-      <div className="fixed bottom-2 right-2 z-50">
-        <Darkmode />
-      </div>
+      <div className="fixed bottom-2 right-2 z-50">{/* <Darkmode /> */}</div>
     </>
   );
 }
